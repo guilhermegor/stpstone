42205c148ee66f704adf38a6c68125bbfcc1facf
Rodrigo Prado <royopa@gmail.com>
2025-02-01 18:06:32 -0300

    Update _global_slots.py
    Refactored the file to improve visualization of code and avoid errors other SO, except Windows

--- stpstone/settings/_global_slots.py
+++ stpstone/settings/_global_slots.py
@@ -4,27 +4,16 @@ import os
 from stpstone.opening_config.setup import reading_yaml
 
 # slots of memory to each yaml
-YAML_ANBIMA = reading_yaml(r'{}\anbima.yaml'.format(
-    os.path.dirname(os.path.realpath(__file__))))
-YAML_B3 = reading_yaml(r'{}\b3.yaml'.format(
-    os.path.dirname(os.path.realpath(__file__))))
-YAML_BR_TRS = reading_yaml(r'{}\br_treasury.yaml'.format(
-    os.path.dirname(os.path.realpath(__file__))))
-YAML_CD = reading_yaml(r'{}\comdinheiro.yaml'.format(
-    os.path.dirname(os.path.realpath(__file__))))
-YAML_GEN = reading_yaml(r'{}\generic.yaml'.format(
-    os.path.dirname(os.path.realpath(__file__))))
-YAML_WGBD = reading_yaml(r'{}\world_gov_bonds.yaml'.format(
-    os.path.dirname(os.path.realpath(__file__))))
-YAML_GLB_RT = reading_yaml(r'{}\global_rates.yaml'.format(
-    os.path.dirname(os.path.realpath(__file__))))
-YAML_USA_MACRO = reading_yaml(r'{}\usa_macro.yaml'.format(
-    os.path.dirname(os.path.realpath(__file__))))
-YAML_BR_MACRO = reading_yaml(r'{}\br_macro.yaml'.format(
-    os.path.dirname(os.path.realpath(__file__))))
-YAML_MICROSOFT_APPS = reading_yaml(r'{}\microsoft_apps.yaml'.format(
-    os.path.dirname(os.path.realpath(__file__))))
-YAML_LLMS = reading_yaml(r'{}\llms.yaml'.format(
-    os.path.dirname(os.path.realpath(__file__))))
-YAML_SESSION = reading_yaml(r'{}\session.yaml'.format(
-    os.path.dirname(os.path.realpath(__file__))))
+PATH = os.path.dirname(os.path.realpath(__file__))
+YAML_ANBIMA = reading_yaml(os.path.join(PATH, 'anbima.yaml'))
+YAML_B3 = reading_yaml(os.path.join(PATH, 'b3.yaml'))
+YAML_BR_MACRO = reading_yaml(os.path.join(PATH, 'br_macro.yaml'))
+YAML_BR_TRS = reading_yaml(os.path.join(PATH, 'br_treasury.yaml'))
+YAML_CD = reading_yaml(os.path.join(PATH, 'comdinheiro.yaml'))
+YAML_GLB_RT = reading_yaml(os.path.join(PATH, 'global_rates.yaml'))
+YAML_GEN = reading_yaml(os.path.join(PATH, 'generic.yaml'))
+YAML_LLMS = reading_yaml(os.path.join(PATH, 'llms.yaml'))
+YAML_MICROSOFT_APPS = reading_yaml(os.path.join(PATH, 'microsoft_apps.yaml'))
+YAML_SESSION = reading_yaml(os.path.join(PATH, 'session.yaml'))
+YAML_USA_MACRO = reading_yaml(os.path.join(PATH, 'usa_macro.yaml'))
+YAML_WGBD = reading_yaml(os.path.join(PATH, 'world_gov_bonds.yaml'))
