6340cf106bb5590a807537e9c960ee6ee46470c3
Guilherme Rodrigues <guirodrigues.gor@gmail.com>
2025-03-02 16:13:11 -0300

    feat: cvm registries added - incentivized companies, securities consultants, crowdfunding, and funds registries

--- examples/cvm_registries.py
+++ examples/cvm_registries.py
@@ -1,19 +1,17 @@
 # pypi.org libs
 import os
 import sys
+
 # local libs
 sys.path.append(os.path.abspath(os.path.join(os.path.realpath(__file__), '..', '..')))
 from stpstone.ingestion.countries.br.registries.cvm import CVMRegistries
 from stpstone.utils.cals.handling_dates import DatesBR
 from stpstone.utils.connections.netops.session import ReqSession
-
+from stpstone.utils.parsers.folders import DirFilesManagement
 
 session = ReqSession(bl_new_proxy=False).session
 
-cls_ = CVMRegistries(
-    session=None,
-    cls_db=None
-)
+cls_ = CVMRegistries(session=None, cls_db=None)
 
 # df_ = cls_.source('independent_fin_advs', bl_fetch=True, bl_debug=False)
 # print(f'\n*** DF INDEPENDENT FINANCIAL ADVISORS - CVM: \n{df_}')
@@ -31,6 +29,23 @@ cls_ = CVMRegistries(
 # print(f'\n*** DF FOREIGN COMPANIES COMPANIES - CVM: \n{df_}')
 # df_.info()
 
-df_ = cls_.source('incentivized_cos', bl_fetch=True, bl_debug=False)
-print(f'\n*** DF INVENTIVIZED COMPANIES COMPANIES - CVM: \n{df_}')
+# df_ = cls_.source('incentivized_cos', bl_fetch=True, bl_debug=False)
+# print(f'\n*** DF INVENTIVIZED COMPANIES COMPANIES - CVM: \n{df_}')
+# df_.info()
+
+# df_ = cls_.source('securities_consultants', bl_fetch=True, bl_debug=False)
+# print(f'\n*** DF SECURITIES CONSULTANTS - CVM: \n{df_}')
+# df_.info()
+# df_.to_excel(
+#     rf'{DirFilesManagement().find_project_root()}\data\cvm-securities-consultants_'
+#     + rf'{DatesBR().curr_date.strftime('%Y%m%d')}_{DatesBR().curr_time.strftime('%H%M%S')}.xlsx',
+#     index=False
+# )
+
+# df_ = cls_.source('crowdfunding', bl_fetch=True, bl_debug=False)
+# print(f'\n*** DF CROWDFUNDING - CVM: \n{df_}')
+# df_.info()
+
+df_ = cls_.source('funds_registries', bl_fetch=True, bl_debug=False)
+print(f'\n*** DF FUNDS REGISTRIES - CVM: \n{df_}')
 df_.info()
