972c06f935bda6fb66b217163fe7e9d52da62ba2
Guilherme Rodrigues <guirodrigues.gor@gmail.com>
2025-03-22 08:31:14 -0300

    feat: dsa deque

--- stpstone/dsa/queues/simple_deque.py
+++ stpstone/dsa/queues/simple_deque.py
@@ -7,6 +7,7 @@ class Deque:
     def __init__(self) -> None:
         self._items: deque[Any] = deque()
 
+    @property
     def is_empty(self) -> bool:
         return not self._items
 
@@ -16,11 +17,14 @@ class Deque:
     def add_rear(self, item: Any) -> None:
         self._items.appendleft(item)
 
+    @property
     def remove_front(self) -> Any:
         return self._items.pop()
 
+    @property
     def remove_rear(self) -> Any:
         return self._items.popleft()
 
+    @property
     def size(self) -> int:
         return len(self._items)
