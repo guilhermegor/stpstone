85b0af07e29be053f22b61051da5caeeb853973a
Guilherme Rodrigues <guirodrigues.gor@gmail.com>
2025-03-30 12:27:55 -0300

    feat: concrete product anbima data debentures infos added

--- examples/anbima_data_debentures_infos.py
+++ examples/anbima_data_debentures_infos.py
@@ -1,8 +1,16 @@
 from stpstone.ingestion.countries.br.registries.anbima_data_debentures import AnbimaDataDebentures
 from stpstone.utils.connections.netops.sessions.proxy_nova import ProxyNova
+from stpstone.utils.connections.netops.user_agents import UserAgents
+
 
 session = ProxyNova("br").sessions
 
 cls_ = AnbimaDataDebentures(
-
+    session=session,
+    list_slugs=["AEAM22", "AALM11"],
+    str_user_agent=UserAgents().get_random_user_agent
 )
+
+df_deb_infos = cls_.source("debentures_registries", bl_fetch=True, bl_debug=False)
+print(f"DF: \n{df_deb_infos}")
+df_deb_infos.info()
