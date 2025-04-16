635415a9d88dc6399eaaa1a7b4269a79d8df7d1f
Guilherme Rodrigues <guirodrigues.gor@gmail.com>
2025-04-16 19:19:17 -0300

    chore: changed examples/investment_funds_bylaws.py to skip backed-up files

--- examples/investment_funds_bylaws.py
+++ examples/investment_funds_bylaws.py
@@ -2,9 +2,11 @@ import pandas as pd
 from getpass import getuser
 from time import sleep
 from random import shuffle
+from typing import List
 from stpstone.ingestion.countries.br.bylaws.investment_funds import InvestmentFundsBylawsBR
 from stpstone.utils.cals.handling_dates import DatesBR
 from stpstone.utils.parsers.lists import HandlingLists
+from stpstone.utils.parsers.folders import DirFilesManagement
 
 
 def clean_excel_text(text):
@@ -13,15 +15,34 @@ def clean_excel_text(text):
     # remove control characters and other problematic chars
     return ''.join(char for char in text if 31 < ord(char) < 127 or char in '\t\n\r')
 
-int_chunk = 200
+def investment_funds_bkpd() -> List[str]:
+    list_ser = list()
+    list_files = DirFilesManagement().loop_files_w_rule(
+        "data", "investment-funds-bylaws-infos_*.xlsx", False, False
+    )
+    for file in list_files:
+        complete_path = rf"data/{file}"
+        reader = pd.read_excel(complete_path)
+        df_ = pd.DataFrame(reader)
+        if df_.empty == True: continue
+        list_ser.extend(df_.to_dict(orient="records"))
+    df_ = pd.DataFrame(list_ser)
+    return df_["URL_SLUG"].unique()
+
+int_chunk = 50
 list_ser = list()
+
+list_slugs_consulted = investment_funds_bkpd()
 
 df_ = pd.read_excel("data/input-funds-regex-bylaws.xlsx")
 df_['URL_SLUG'] = df_['URL Regulamento'].str.replace(
     'https://web.cvm.gov.br/app/fundosweb/fundos/regulamento/obter/por/arquivo/',
     ''
 )
-list_slugs = df_['URL_SLUG'].tolist()[:3000]
+list_slugs = df_['URL_SLUG'].tolist()
+print(f"Slugs before filtering: {len(list_slugs)}")
+list_slugs = list(set(list_slugs) - set(list_slugs_consulted))
+print(f"Slugs after filtering: {len(list_slugs)}")
 shuffle(list_slugs)
 list_chunks = HandlingLists().chunk_list(list_slugs, None, int_chunk)
 
