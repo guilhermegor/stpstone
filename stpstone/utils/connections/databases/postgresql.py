81d108d359c967681324610817fb15c2fe43fad7
guilhermegor <guirodrigues.gor@gmail.com>
2025-05-21 12:27:48 -0300

    feat: abc for databases engines
    - added abc.py module with conn and cursor (as enforced class variables) to the __init__
    - abstract methods require: execute, read, insert, close and backup
    - __enter__ and __exit__ as concrete methods common to all implementations

--- stpstone/utils/connections/databases/postgresql.py
+++ stpstone/utils/connections/databases/postgresql.py
@@ -9,25 +9,13 @@ from typing import List, Dict, Any, Optional
 from stpstone.utils.cals.handling_dates import DatesBR
 from stpstone.utils.parsers.json import JsonFiles
 from stpstone.utils.loggs.create_logs import CreateLog
+from stpstone.utils.connections.databases.abc import DatabaseConnection
 
 
-class PostgreSQLDB:
+class PostgreSQLDB(DatabaseConnection):
 
     def __init__(self, dbname:str, user:str, password:str, host:str, port:int,
-                 str_schema:str='public', logger:Optional[Logger]=None) \
-        -> None:
-        """
-        DOCSTRING: INITIALIZES THE CONNECTION TO THE POSTGRESQL DATABASE
-        INPUTS:
-            - DBNAME:STR
-            - USER:STR
-            - PASSWORD:STR
-            - HOST:STR
-            - PORT:INT
-            - SCHEMA:STR
-            - LOGGER:OPTIONAL[LOGGER]
-        OUTPUTS: -
-        """
+                 str_schema:str="public", logger:Optional[Logger]=None) -> None:
         self.dbname = dbname
         self.user = user
         self.password = password
@@ -36,31 +24,21 @@ class PostgreSQLDB:
         self.str_schema = str_schema
         self.logger = logger
         self.dict_db_config = {
-            'dbname': self.dbname,
-            'user': self.user,
-            'password': self.password,
-            'host': self.host,
-            'port': self.port
+            "dbname": self.dbname,
+            "user": self.user,
+            "password": self.password,
+            "host": self.host,
+            "port": self.port
         }
         self.conn: Connection = psycopg2.connect(**self.dict_db_config)
         self.cursor: Cursor = self.conn.cursor()
         self.execute(f"SET search_path TO '{self.str_schema}';")
 
     def execute(self, str_query:str) -> None:
-        """
-        DOCSTRING: RUN QUERY WITH DML ACCESS
-        INPUTS: QUERY
-        OUTPUTS: -
-        """
         self.cursor.execute(str_query)
 
     def read(self, str_query:str, dict_type_cols:Optional[Dict[str, Any]]=None,
               list_cols_dt:Optional[List[str]]=None, str_fmt_dt:Optional[str]=None) -> pd.DataFrame:
-        """
-        DOCSTRING: RUN QUERY AND RETURN RESULTS AS DATAFRAME
-        INPUTS: QUERY, DICT_TYPE_COLS, LIST_COLS_DT, STR_FMT_DT
-        OUTPUTS: Pandas DataFrame
-        """
         # retrieving dataframe
         df_ = pd.read_sql_query(str_query, self.conn)
         # changing data types
@@ -73,22 +51,17 @@ class PostgreSQLDB:
 
     def insert(self, json_data:List[Dict[str, Any]], str_table_name:str,
         bl_insert_or_ignore:bool=False) -> None:
-        """
-        DOCSTRING: INSERTS DATA FROM A JSON OBJECT INTO A POSTGRESQL TABLE
-        INPUTS: JSON_DATA
-        OUTPUTS: -
-        """
         # validate json, in order to have the same keys
         json_data = JsonFiles().normalize_json_keys(json_data)
         # sql insert statement
-        list_columns = ', '.join(json_data[0].keys())
-        list_placeholders = ', '.join(['%s' for _ in json_data[0]])
+        list_columns = ", ".join(json_data[0].keys())
+        list_placeholders = ", ".join(["%s" for _ in json_data[0]])
         if bl_insert_or_ignore == True:
-            str_query = f'INSERT INTO {str_table_name} ({list_columns}) VALUES ' \
-                + f'({list_placeholders}) ON CONFLICT DO NOTHING'
+            str_query = f"INSERT INTO {str_table_name} ({list_columns}) VALUES " \
+                + f"({list_placeholders}) ON CONFLICT DO NOTHING"
         else:
-            str_query = f'INSERT INTO {str_table_name} ({list_columns}) VALUES ' \
-                + f'({list_placeholders})'
+            str_query = f"INSERT INTO {str_table_name} ({list_columns}) VALUES " \
+                + f"({list_placeholders})"
         try:
             execute_values(
                 self.cursor,
@@ -99,8 +72,8 @@ class PostgreSQLDB:
             if self.logger is not None:
                 CreateLog().info(
                     self.logger,
-                    f'Successful commit in db {self.dict_db_config["dbname"]} '
-                    + f'/ table {str_table_name}.'
+                    f"Successful commit in db {self.dict_db_config["dbname"]} "
+                    + f"/ table {str_table_name}."
                 )
         except Exception as e:
             self.conn.rollback()
@@ -108,57 +81,45 @@ class PostgreSQLDB:
             if self.logger is not None:
                 CreateLog().error(
                     self.logger,
-                    'ERROR WHILE INSERTING DATA\n'
-                    + f'DB_CONFIG: {self.dict_db_config}\n'
-                    + f'TABLE_NAME: {str_table_name}\n'
-                    + f'JSON_DATA: {json_data}\n'
-                    + f'ERROR_MESSAGE: {e}'
+                    "ERROR WHILE INSERTING DATA\n"
+                    + f"DB_CONFIG: {self.dict_db_config}\n"
+                    + f"TABLE_NAME: {str_table_name}\n"
+                    + f"JSON_DATA: {json_data}\n"
+                    + f"ERROR_MESSAGE: {e}"
                 )
             raise Exception(
-                'ERROR WHILE INSERTING DATA\n'
-                + f'DB_CONFIG: {self.dict_db_config}\n'
-                + f'TABLE_NAME: {str_table_name}\n'
-                + f'JSON_DATA: {json_data}\n'
-                + f'ERROR_MESSAGE: {e}'
+                "ERROR WHILE INSERTING DATA\n"
+                + f"DB_CONFIG: {self.dict_db_config}\n"
+                + f"TABLE_NAME: {str_table_name}\n"
+                + f"JSON_DATA: {json_data}\n"
+                + f"ERROR_MESSAGE: {e}"
             )
 
-    @property
     def close(self) -> None:
-        """
-        DOCSTRING: CLOSES THE CONNECTION TO THE DATABASE
-        INPUTS: -
-        OUTPUTS: -
-        """
         self.conn.close()
 
-    def _bkp_db(self, str_backup_dir:str, str_bkp_name:str=None) -> str:
-        """
-        DOCSTRING: BACKUP DATABASE - NEED PG_DUMP TO BE CONFIGURED WITHIN ENVIRONMENT VARIABLES
-        INPUTS:
-            - BACKUP_DIR (STR): DIRECTORY WHERE THE BACKUP FILE WILL BE SAVED
-        OUTPUTS: STR: PATH TO THE BACKUP FILE, OR AN ERROR MESSAGE.
-        """
+    def backup(self, str_backup_dir:str, str_bkp_name:str=None) -> str:
         try:
             # ensure the backup directory exists
             os.makedirs(str_backup_dir, exist_ok=True)
             # generate the backup file name
             backup_file = os.path.join(str_backup_dir, str_bkp_name)
             env = os.environ.copy()
-            env['PGPASSWORD'] = self.password
+            env["PGPASSWORD"] = self.password
             # run the pg_dump command
             command = [
-                'pg_dump',
-                '-h', self.host,
-                '-p', str(self.port),
-                '-U', self.user,
-                '-F', 'c',  # custom format
-                '-b',       # include large objects
-                '-f', backup_file,  # output file
+                "pg_dump",
+                "-h", self.host,
+                "-p", str(self.port),
+                "-U", self.user,
+                "-F", "c",  # custom format
+                "-b",       # include large objects
+                "-f", backup_file,  # output file
                 self.dbname
             ]
             subprocess.run(command, check=True, env=env)
-            return f'Backup successful! File saved at: {backup_file}'
+            return f"Backup successful! File saved at: {backup_file}"
         except subprocess.CalledProcessError as e:
-            return f'Backup failed: {e}'
+            return f"Backup failed: {e}"
         except Exception as e:
-            return f'An error occurred: {e}'
+            return f"An error occurred: {e}"
