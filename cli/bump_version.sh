1b780c41a61bc7b4b9b482eabce8db27fa860240
guilhermegor <guirodrigues.gor@gmail.com>
2025-06-23 08:18:53 -0300

    bugfix: python package version in __init__.py file

--- cli/bump_version.sh
+++ cli/bump_version.sh
@@ -26,6 +26,32 @@ get_current_version() {
     poetry version -s
 }
 
+update_init_version() {
+    local new_version=$1
+    local init_file="stpstone/__init__.py"
+    local backup_file="${init_file}.bak"
+    
+    if [ ! -f "$init_file" ]; then
+        print_status "error" "File $init_file not found!"
+        return 1
+    fi
+    
+    sed -i.bak -E "s/(__version__ = \")[0-9]+\.[0-9]+\.[0-9]+(\")/\1${new_version}\2/" "$init_file"
+    
+    if grep -q "__version__ = \"${new_version}\"" "$init_file"; then
+        print_status "success" "Updated fallback version in ${init_file} to ${new_version}"
+        if [ -f "$backup_file" ]; then
+            rm -f "$backup_file"
+            print_status "debug" "Removed backup file ${backup_file}"
+        fi
+        return 0
+    else
+        print_status "error" "Failed to update version in ${init_file}"
+        print_status "warning" "Backup file preserved at ${backup_file}"
+        return 1
+    fi
+}
+
 select_version_bump() {
     print_status "info" "Current version: $(get_current_version)"
     echo -e "${YELLOW}Select version bump type:${NC}"
@@ -70,6 +96,10 @@ main() {
     
     new_version=$(get_current_version)
     print_status "success" "New version: ${new_version}"
+    
+    if ! update_init_version "$new_version"; then
+        exit 1
+    fi
     
     print_status "info" "Updating dependencies..."
     poetry update
