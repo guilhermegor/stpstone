f07817fb1ff83c85add5eec23836a95f6266c3b9
guilhermegor <guirodrigues.gor@gmail.com>
2025-06-23 07:06:06 -0300

    bugfix: added package version metadata information

--- stpstone/__init__.py
+++ stpstone/__init__.py
@@ -1,1 +1,5 @@
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+from importlib.metadata import version
+
+__version__ = version(__name__)
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
