4128e3999ba4b9af6d9a6e15043da93655093d50
guilhermegor <guirodrigues.gor@gmail.com>
2025-08-18 14:31:35 -0300

    refactor: proxy_webshare

--- stpstone/utils/connections/netops/proxies/_free/proxy_webshare.py
+++ stpstone/utils/connections/netops/proxies/_free/proxy_webshare.py
@@ -126,7 +126,8 @@ class ProxyWebShare(ABCSession):
             "user-agent": (
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                 "(KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
-            )
+            ), 
+            "Cookie": "_gcl_au=1.1.915571832.1743243656; _tid=6ec40fde-5dd2-4d9e-a5d5-aee93aae48fb; _ga=GA1.1.20341267.1743243657; _did=20341267.1743243657; _ir=no-referrer; _gcl_gs=2.1.k1$i1743495693$u101187463; _gcl_aw=GCL.1743495703.Cj0KCQjwna6_BhCbARIsALId2Z2IXgfZkTk_BdDu2ioMsUOLTrX8_M-gxCu9R0-38WABC4hnnQpQR4kaAiz0EALw_wcB; _sid=1743495703; ssotoken=1yzdyxhh7yg3dv5v17d5uxpsixz5xzfb9w7l64i1; newDesignLoginToken=1yzdyxhh7yg3dv5v17d5uxpsixz5xzfb9w7l64i1; intercom-session-zsppwl0f=NkM0aFo4Q0hzSWdtRllHQnJiUjlrbCtWSkxBTWJMaFlWRlVzWW1nUG5MZ0FZMnBhQ0ROWXZ1Nzk0UFkrZnZsSkNZZjVtWEd2dFdnbi9XclZlK3djR2d1UWxlaUlXZmdyUzZqb3dLeEJ5N1E9LS0wV3k5MXNSdXRJWXhsdC9EQkF4a0ZRPT0=--1187e227a07523f74ece39ca541dbf9d2c16d830; intercom-device-id-zsppwl0f=6eda0713-af9b-486f-9a17-321880576b16; _ga_Z1CFG0XGWL=GS1.1.1743495703.4.1.1743495796.28.1.1316803691; ph_phc_SgStpNtFchAMqb1IKSAIPiDKGdGrkEYEap1wqhRjcD8_posthog=%7B%22distinct_id%22%3A%226ec40fde-5dd2-4d9e-a5d5-aee93aae48fb%22%2C%22%24sesid%22%3A%5B1743495808069%2C%220195f071-28ba-7bbf-9736-107b86a56fc6%22%2C1743495702714%5D%2C%22%24epp%22%3Atrue%2C%22%24initial_person_info%22%3A%7B%22r%22%3A%22https%3A%2F%2Fwww.google.com%2F%22%2C%22u%22%3A%22https%3A%2F%2Fwww.webshare.io%2Facademy-article%2Fselenium-proxy%22%7D%7D; _tid=6ec40fde-5dd2-4d9e-a5d5-aee93aae48fb" # noqa E501: line too long
         }
 
     def _available_proxies(self) -> list[ReturnAvailableProxies]:
@@ -143,17 +144,17 @@ class ProxyWebShare(ABCSession):
             If API request fails or returns invalid data
         """
         try:
-            headers = self._get_request_headers()
             resp_req = request(
                 "GET",
                 self.fstr_url.format(self.str_plan_id),
-                headers=headers,
-                data={}
+                headers=self._get_request_headers(),
+                data={}, 
+                timeout=10
             )
             resp_req.raise_for_status()
             json_data = resp_req.json()
 
-            proxies = []
+            list_proxies = []
             for proxy in json_data.get("results", []):
                 country_code = proxy.get("country_code", "")
                 timezones = ", ".join(
@@ -201,8 +202,8 @@ class ProxyWebShare(ABCSession):
                     "ratio_times_alive_dead": 1.0,
                     "uptime": 1.0
                 }
-                proxies.append(proxy_data)
+                list_proxies.append(proxy_data)
 
-            return proxies
+            return list_proxies
         except Exception as err:
             raise ValueError(f"Failed to fetch or process proxies: {str(err)}") from err
