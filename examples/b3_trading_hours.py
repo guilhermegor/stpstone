dfe24a1a93ac50d80d02de0465a85b9d85d40333
guilhermegor <guirodrigues.gor@gmail.com>
2025-09-13 06:43:06 -0300

    feat: b3 trading hours - stock index futures and pmi futures

--- examples/b3_trading_hours.py
+++ examples/b3_trading_hours.py
@@ -2,8 +2,43 @@
 
 from stpstone.ingestion.countries.br.exchange.b3_trading_hours import (
     B3TradingHoursOptionsExercise,
+    B3TradingHoursPMIFutures,
+    B3TradingHoursStockIndexFutures,
     B3TradingHoursStocks,
 )
+
+
+cls_ = B3TradingHoursPMIFutures(
+    date_ref=None,
+    logger=None, 
+    cls_db=None
+)
+
+df_ = cls_.run(bool_verify=False)
+print(f"DF B3 TRADING HOURS PMI FUTURES: \n{df_}")
+df_.info()
+
+
+cls_ = B3TradingHoursStockIndexFutures(
+    date_ref=None,
+    logger=None, 
+    cls_db=None
+)
+
+df_ = cls_.run(bool_verify=False)
+print(f"DF B3 TRADING HOURS STOCK INDEX FUTURES: \n{df_}")
+df_.info()
+
+
+cls_ = B3TradingHoursStockIndexFutures(
+    date_ref=None,
+    logger=None, 
+    cls_db=None
+)
+
+df_ = cls_.run(bool_verify=False)
+print(f"DF B3 TRADING HOURS STOCK INDEX FUTURES: \n{df_}")
+df_.info()
 
 
 cls_ = B3TradingHoursStocks(
