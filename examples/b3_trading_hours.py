d2264f89ce432bce800cc8c9f1a361a62c0582d6
guilhermegor <guirodrigues.gor@gmail.com>
2025-09-13 07:01:22 -0300

    b3 trading futures - commodities futures

--- examples/b3_trading_hours.py
+++ examples/b3_trading_hours.py
@@ -1,6 +1,7 @@
 """B3 Trading Hours."""
 
 from stpstone.ingestion.countries.br.exchange.b3_trading_hours import (
+    B3TradingHoursCommoditiesFutures,
     B3TradingHoursOptionsExercise,
     B3TradingHoursPMIFutures,
     B3TradingHoursRealDenominatedInterestRates,
@@ -8,6 +9,17 @@ from stpstone.ingestion.countries.br.exchange.b3_trading_hours import (
     B3TradingHoursStocks,
     B3TradingHoursUSDollarDenominatedInterestRatesFutures,
 )
+
+
+cls_ = B3TradingHoursCommoditiesFutures(
+    date_ref=None,
+    logger=None, 
+    cls_db=None
+)
+
+df_ = cls_.run(bool_verify=False)
+print(f"DF B3 TRADING HOURS COMMODITIES FUTURES: \n{df_}")
+df_.info()
 
 
 cls_ = B3TradingHoursUSDollarDenominatedInterestRatesFutures(
