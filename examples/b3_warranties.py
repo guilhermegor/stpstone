3197e5c4dbcf6f4ed09f0caada51c3cd8b14b952
guilhermegor <guirodrigues.gor@gmail.com>
2025-09-13 21:38:23 -0300

    feat: b3 warranties - international securities

--- examples/b3_warranties.py
+++ examples/b3_warranties.py
@@ -1,6 +1,20 @@
 """B3 Warranties."""
 
-from stpstone.ingestion.countries.br.exchange.b3_warranties import B3WarrantiesStocksUnitsETFs
+from stpstone.ingestion.countries.br.exchange.b3_warranties import (
+    B3WarrantiesBRSovereignBonds,
+    B3WarrantiesStocksUnitsETFs,
+)
+
+
+cls_ = B3WarrantiesBRSovereignBonds(
+    date_ref=None,
+    logger=None, 
+    cls_db=None
+)
+
+df_ = cls_.run(bool_verify=False)
+print(f"DF B3 WARRANTIES BR SOVEREIGN BONDS: \n{df_}")
+df_.info()
 
 
 cls_ = B3WarrantiesStocksUnitsETFs(
@@ -12,4 +26,3 @@ cls_ = B3WarrantiesStocksUnitsETFs(
 df_ = cls_.run(bool_verify=False)
 print(f"DF B3 WARRANTIES STOCKS UNITS ETFs: \n{df_}")
 df_.info()
-
