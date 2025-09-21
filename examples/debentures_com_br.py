5ff117cda9d60bb8cee35579455850e2c0ec530c
guilhermegor <guirodrigues.gor@gmail.com>
2025-09-21 07:09:13 -0300

    feat: debentures_com_br - otc pvs

--- examples/debentures_com_br.py
+++ examples/debentures_com_br.py
@@ -2,8 +2,21 @@
 from stpstone.ingestion.countries.br.otc.debentures_com_br import (
     DebenturesComBrInfos,
     DebenturesComBrMTM,
+    DebenturesComBrOTCPVs,
 )
 from stpstone.utils.calendars.calendar_br import DatesBRAnbima
+
+
+cls_ = DebenturesComBrOTCPVs(
+    date_start=None,
+    date_end=None,
+    logger=None,
+    cls_db=None
+)
+
+df_ = cls_.run(bool_verify=False)
+print(f"DF DEBENTURES COM BR OTC PVs: \n{df_}")
+df_.info()
 
 
 cls_ = DebenturesComBrInfos(
--- stpstone/ingestion/countries/br/otc/debentures_com_br.py
+++ stpstone/ingestion/countries/br/otc/debentures_com_br.py
@@ -547,3 +547,191 @@ class DebenturesComBrInfos(ABCIngestionOperations):
             na_values=["-", "-  ", "  -", " -", " - ", " "],
             engine="python",
         )
+
+
+class DebenturesComBrOTCPVs(ABCIngestionOperations):
+    """Debentures.com.br OTC PVs.
+    
+    Notes
+    -----
+    [1] Metadata: https://www.debentures.com.br/exploreosnd/consultaadados/mercadosecundario/precosdenegociacao_f.asp?op_exc=Nada
+    """
+    
+    def __init__(
+        self, 
+        date_start: Optional[date] = None,
+        date_end: Optional[date] = None,
+        logger: Optional[Logger] = None,
+        cls_db: Optional[Session] = None,
+    ) -> None:
+        """Initialize the ingestion class.
+        
+        Parameters
+        ----------
+        date_ref : Optional[date], optional
+            The date of reference, by default None.
+        logger : Optional[Logger], optional
+            The logger, by default None.
+        cls_db : Optional[Session], optional
+            The database session, by default None.
+        
+        Returns
+        -------
+        None
+        """
+        super().__init__(cls_db=cls_db)
+        CoreIngestion.__init__(self)
+        ContentParser.__init__(self)
+
+        self.logger = logger
+        self.cls_db = cls_db
+        self.cls_dir_files_management = DirFilesManagement()
+        self.cls_dates_current = DatesCurrent()
+        self.cls_create_log = CreateLog()
+        self.cls_dates_br = DatesBRAnbima()
+        self.date_start = date_start or \
+            self.cls_dates_br.add_working_days(self.cls_dates_current.curr_date(), -5)
+        self.date_end = date_end or \
+            self.cls_dates_br.add_working_days(self.cls_dates_current.curr_date(), -1)
+        self.url = "https://www.debentures.com.br/exploreosnd/consultaadados/mercadosecundario/precosdenegociacao_e.asp?op_exc=Nada&emissor=&isin=&ativo=&dt_ini={}&dt_fim={}"
+    
+    def run(
+        self,
+        timeout: Optional[Union[int, float, tuple[float, float], tuple[int, int]]] = (12.0, 21.0),
+        bool_verify: bool = True,
+        bool_insert_or_ignore: bool = False, 
+        str_table_name: str = "br_debentures_com_br_mtm"
+    ) -> Optional[pd.DataFrame]:
+        """Run the ingestion process.
+        
+        If the database session is provided, the data is inserted into the database.
+        Otherwise, the transformed DataFrame is returned.
+
+        Parameters
+        ----------
+        timeout : Optional[Union[int, float, tuple[float, float], tuple[int, int]]], optional
+            The timeout, by default (12.0, 21.0)
+        bool_verify : bool, optional
+            Whether to verify the SSL certificate, by default True
+        bool_insert_or_ignore : bool, optional
+            Whether to insert or ignore the data, by default False
+        str_table_name : str, optional
+            The name of the table, by default "br_debentures_com_br_mtm"
+
+        Returns
+        -------
+        Optional[pd.DataFrame]
+            The transformed DataFrame.
+        """
+        resp_req = self.get_response(timeout=timeout, bool_verify=bool_verify)
+        file = self.parse_raw_file(resp_req)
+        df_ = self.transform_data(file=file)
+        df_ = self.standardize_dataframe(
+            df_=df_, 
+            date_ref=self.date_end,
+            dict_dtypes={
+                "DATA": "date", 
+                "EMISSOR": str, 
+                "CODIGO_DO_ATIVO": str, 
+                "ISIN": str, 
+                "QUANTIDADE": int, 
+                "NUMERO_DE_NEGOCIOS": int, 
+                "PU_MINIMO": float, 
+                "PU_MEDIO": float, 
+                "PU_MAXIMO": float, 
+                "PCT_PU_DA_CURVA": float,
+            }, 
+            str_fmt_dt="DD/MM/YYYY",
+            url=self.url,
+        )
+        if self.cls_db:
+            self.insert_table_db(
+                cls_db=self.cls_db, 
+                str_table_name=str_table_name, 
+                df_=df_, 
+                bool_insert_or_ignore=bool_insert_or_ignore
+            )
+        else:
+            return df_
+
+    @backoff.on_exception(
+        backoff.expo, 
+        requests.exceptions.HTTPError, 
+        max_time=60
+    )
+    def get_response(
+        self, 
+        timeout: Optional[Union[int, float, tuple[float, float], tuple[int, int]]] = (12.0, 21.0), 
+        bool_verify: bool = True
+    ) -> Union[Response, PlaywrightPage, SeleniumWebDriver]:
+        """Return a list of response objects.
+
+        Parameters
+        ----------
+        timeout : Optional[Union[int, float, tuple[float, float], tuple[int, int]]], optional
+            The timeout, by default (12.0, 21.0)
+        bool_verify : bool, optional
+            Verify the SSL certificate, by default True
+        
+        Returns
+        -------
+        Union[Response, PlaywrightPage, SeleniumWebDriver]
+            A list of response objects.
+        """
+        url = self.url.format(
+            self.date_start.strftime("%Y%m%d"),
+            self.date_end.strftime("%Y%m%d"),
+        )
+        resp_req = requests.get(url, timeout=timeout, verify=bool_verify)
+        resp_req.raise_for_status()
+        return resp_req
+    
+    def parse_raw_file(
+        self, 
+        resp_req: Union[Response, PlaywrightPage, SeleniumWebDriver]
+    ) -> StringIO:
+        """Parse the raw file content.
+        
+        Parameters
+        ----------
+        resp_req : Union[Response, PlaywrightPage, SeleniumWebDriver]
+            The response object.
+        
+        Returns
+        -------
+        StringIO
+            The parsed content.
+        """
+        return self.get_file(resp_req=resp_req)
+    
+    def transform_data(
+        self, 
+        file: StringIO
+    ) -> pd.DataFrame:
+        """Transform a list of response objects into a DataFrame.
+        
+        Parameters
+        ----------
+        file : StringIO
+            The parsed content.
+        
+        Returns
+        -------
+        pd.DataFrame
+            The transformed DataFrame.
+        """
+        return pd.read_csv(
+            file, 
+            sep="\t", 
+            skiprows=3, 
+            names=["DATA", "EMISSOR", "CODIGO_DO_ATIVO", "ISIN", "QUANTIDADE", 
+                   "NUMERO_DE_NEGOCIOS", "PU_MINIMO", "PU_MEDIO", "PU_MAXIMO", 
+                   "PCT_PU_DA_CURVA"],
+            header=None, 
+            decimal=",", 
+            thousands=".", 
+            encoding="latin-1",
+            na_values=["-", "-  ", " ", "ND"],
+            skipfooter=4,
+            engine="python",
+        )
