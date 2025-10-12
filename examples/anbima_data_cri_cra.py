a680aec85ff838a3a05cf49ec896a86ea0d61082
guilhermegor <guirodrigues.gor@gmail.com>
2025-10-12 17:50:08 -0300

    feat: anbima data cri cra - individual characteristics

--- examples/anbima_data_cri_cra.py
+++ examples/anbima_data_cri_cra.py
@@ -2,43 +2,56 @@
 
 from stpstone.ingestion.countries.br.registries.anbima_data_cri_cra import (
     AnbimaDataCRICRACharacteristics,
+    AnbimaDataCRICRAIndividualCharacteristics,
     AnbimaDataCRICRAPricesFile,
     AnbimaDataCRICRAPricesWS,
 )
 
 
-cls_ = AnbimaDataCRICRAPricesFile(
+cls_ = AnbimaDataCRICRAIndividualCharacteristics(
     date_ref=None,
     logger=None,
     cls_db=None,
+    list_asset_codes=["18L1085826", "19C0000001", "CRA019000GT"],
 )
 
 df_ = cls_.run()
-print(f"DF ANBIMA CRI/CRA PRICES: \n{df_}") 
+print(f"DF ANBIMA CRI/CRA INDIVIDUAL CHARACTERISTICS: \n{df_}")
 df_.info()
 
 
-cls_ = AnbimaDataCRICRAPricesWS(
-    date_ref=None,
-    logger=None,
-    cls_db=None,
-    start_page=0,
-    end_page=None,
-)
+# cls_ = AnbimaDataCRICRAPricesFile(
+#     date_ref=None,
+#     logger=None,
+#     cls_db=None,
+# )
 
-df_ = cls_.run()
-print(f"DF ANBIMA CRI/CRA PRICES: \n{df_}") 
-df_.info()
+# df_ = cls_.run()
+# print(f"DF ANBIMA CRI/CRA PRICES: \n{df_}") 
+# df_.info()
 
 
-cls_ = AnbimaDataCRICRACharacteristics(
-    date_ref=None,
-    logger=None,
-    cls_db=None,
-    start_page=0,
-    end_page=None,
-)
+# cls_ = AnbimaDataCRICRAPricesWS(
+#     date_ref=None,
+#     logger=None,
+#     cls_db=None,
+#     start_page=0,
+#     end_page=None,
+# )
 
-df_ = cls_.run()
-print(f"DF ANBIMA CRI/CRA CHARACTERISTICS: \n{df_}")
-df_.info()
+# df_ = cls_.run()
+# print(f"DF ANBIMA CRI/CRA PRICES: \n{df_}") 
+# df_.info()
+
+
+# cls_ = AnbimaDataCRICRACharacteristics(
+#     date_ref=None,
+#     logger=None,
+#     cls_db=None,
+#     start_page=0,
+#     end_page=None,
+# )
+
+# df_ = cls_.run()
+# print(f"DF ANBIMA CRI/CRA CHARACTERISTICS: \n{df_}")
+# df_.info()
