ae7911c7b00ca4afb412a5776c30bd16124ff53c
guilhermegor <guirodrigues.gor@gmail.com>
2025-10-21 18:51:18 -0300

    feat: cvm data - fif monthly profile

--- examples/cvm_data.py
+++ examples/cvm_data.py
@@ -1,14 +1,25 @@
 """Example of how to use FIFDailyInfos class to ingest data from CVM."""
 
-from stpstone.ingestion.countries.br.registries.cvm_data import FIFDailyInfos
+from stpstone.ingestion.countries.br.registries.cvm_data import FIFDailyInfos, FIFMonthlyProfile
 
 
-cls_ = FIFDailyInfos(
+# cls_ = FIFDailyInfos(
+#     date_ref=None, 
+#     logger=None, 
+#     cls_db=None,
+# )
+
+# df_ = cls_.run()
+# print(f"DF CVM DATA: \n{df_}")
+# df_.info()
+
+
+cls_ = FIFMonthlyProfile(
     date_ref=None, 
     logger=None, 
     cls_db=None,
 )
 
 df_ = cls_.run()
-print(f"DF CVM DATA: \n{df_}")
+print(f"DF CVM MONTHLY PROFILE: \n{df_}")
 df_.info()
