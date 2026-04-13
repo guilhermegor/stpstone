af37493473c9d2259be9b6e835d8d31aa3bf2cf7
guihermegor <guirodrigues.gor@gmail.com>
2026-04-13 07:59:17 -0300

    refactor(_config): removed unused YAML files

--- stpstone/_config/global_slots.py
+++ stpstone/_config/global_slots.py
@@ -7,11 +7,5 @@ from stpstone.utils.parsers.yaml import reading_yaml
 
 root_path = os.path.dirname(os.path.realpath(__file__))
 
-# * bylaws
-#   BR
-YAML_INVESTMENT_FUNDS_BYLAWS = reading_yaml(
-    os.path.join(root_path, "countries/br/bylaws/cvm_investment_funds.yaml")
-)
-
 YAML_ANBIMA_DATA_API = reading_yaml(
     os.path.join(root_path, "countries/br/registries/anbima_data_api.yaml"))
